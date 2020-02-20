cbd83b2e599a4b81cd0e2c2321ef8f3e86e4f713
Nate <52894316+NoMudNoLotus@users.noreply.github.com>
2020-02-19 19:54:15 -0500

    Create raspberrybanner.sh

---
+++ raspberrybanner.sh
@@ -0,0 +1,27 @@
+let upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
+let secs=$((${upSeconds}%60))
+let mins=$((${upSeconds}/60%60))
+let hours=$((${upSeconds}/3600%24))
+let days=$((${upSeconds}/86400))
+UPTIME=`printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs"`
+
+# get the load averages
+read one five fifteen rest < /proc/loadavg
+
+# raspnerry graphic
+echo "$(tput setaf 2)
+   .~~.   .~~.    `date +"%A, %e %B %Y, %r"`
+  '. \ ' ' / .'   `uname -srmo`$(tput setaf 1)
+   .~ .~~~..~.
+  : .~.'~'.~. :   Uptime.............: ${UPTIME}
+ ~ (   ) (   ) ~  Memory.............: `cat /proc/meminfo | grep MemFree | awk {'print $2'}`kB (Free) / `cat /proc/meminfo | grep MemTotal | awk {'print $2'}`kB (Total)
+( : '~'.~.'~' : ) Load Averages......: ${one}, ${five}, ${fifteen} (1, 5, 15 min)
+ ~ .~ (   ) ~. ~  Running Processes..: `ps ax | wc -l | tr -d " "`
+  (  : '~' :  )   Pi Files...........: `find . -type f -print | wc -l`
+   '~ .~~~. ~'    CPU Temp...........: `/opt/vc/bin/vcgencmd measure_temp | sed 's/.*=//'`
+       '~'
+$(tput sgr0)"
+
+# rainbow fortune
+echo TODAYS RAINBOW FORTUNE | lolcat -a -d 5
+fortune | cowsay -f $(ls /usr/share/cowsay/cows/ | shuf -n 1) | lolcat -a -d 5
